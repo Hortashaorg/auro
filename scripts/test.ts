4586405dee0d422819136b0fbd40450538418ad9
Hortasha <eidemartin_303@hotmail.com>
2024-12-02 12:02:31 +0000

    non blocking paralell execution

--- scripts/test.ts
+++
@@ -1,9 +0,0 @@
-import { expandGlob } from "@std/fs";
-
-const workspaces = await Array.fromAsync(expandGlob("./**/deno.json"));
-
-for (const workspace of workspaces) {
-    const file = await Deno.readTextFile(workspace.path);
-    const denofile = JSON.parse(file);
-    console.log(denofile.name);
-}
