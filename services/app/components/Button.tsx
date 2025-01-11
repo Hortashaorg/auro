2b77b205ee8fa6fd47f1dd67c8a8296b97111797
Hortasha <eidemartin_303@hotmail.com>
2025-01-11 15:09:22 +0000

    able to do a button

--- services/app/components/Button.tsx
+++ services/app/components/Button.tsx
@@ -1,11 +1,28 @@
+import type { NonNullableProps } from "@utils/types.ts";
+import { cva } from "class-variance-authority";
 import type { JSX } from "preact";
 
-interface Props extends JSX.HTMLAttributes<HTMLButtonElement> {}
-export const Button = ({ children, ...rest }: Props) => {
+const buttonVariants = cva(["bg-primary-50", "hover:bg-primary-100"], {
+  variants: {
+    variant: {
+      primary: ["text-text-700", "font-bold", "py-2", "px-4", "rounded"],
+      secondary: ["text-text-700", "font-normal", "py-2", "px-4", "rounded"],
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  },
+});
+
+type ButtonVariants = NonNullableProps<typeof buttonVariants>;
+
+interface Props extends JSX.HTMLAttributes<HTMLButtonElement>, ButtonVariants {}
+
+export const Button = ({ children, variant, ...rest }: Props) => {
   return (
     <button
       {...rest}
-      className="bg-primary-50 hover:bg-primary-100 text-text-700 font-bold py-2 px-4 rounded"
+      className={buttonVariants({ variant })}
     >
       {children}
     </button>
