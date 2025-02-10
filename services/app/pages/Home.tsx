5b29289f891d229589a0991991df89017965012a
MartinEide <martin.eide@hent.no>
2025-02-10 07:07:29 +0000

    add info component

--- services/app/pages/Home.tsx
+++ services/app/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Layout } from "@layouts/Layout.tsx";
 import { Text } from "@comp/content/Text.tsx";
+import { Info } from "@comp/feedback/Info.tsx";
 
 export const Home = () => {
   return (
     <Layout title="Deno Hot Dude">
       <Text>Home page</Text>
+      <Info variant="warning">
+        <Text>This is a warning</Text>
+      </Info>
     </Layout>
   );
 };
