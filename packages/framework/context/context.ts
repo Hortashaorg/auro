abcab9cbcf8e92a3aa58475bfcd139277f80e1bd
Hortasha <eidemartin_303@hotmail.com>
2025-02-12 20:43:17 +0000

    delete some old stuff

--- packages/framework/context/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "@hono/hono/jsx";
-
-export const RenderContext = createContext<null | Record<string, unknown>>(
-  null,
-);
-
-export const getContext = (): Record<string, unknown> => {
-  return useContext(RenderContext) as Record<string, unknown>;
-};
