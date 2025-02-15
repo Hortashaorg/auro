d068b0ac4ad7a83c68ef9f5b9eae28b6de56d9cb
Hortasha <eidemartin_303@hotmail.com>
2025-02-15 21:10:35 +0000

    make auth provider optional

--- packages/framework/server/app.tsx
+++ packages/framework/server/app.tsx
@@ -56,7 +56,7 @@ type ValidateHookTypes<T extends "google"> = T extends "google" ? {
 /** Init Framework App */
 export const app = <TProvider extends "google">(
   settings: {
-    authProvider: {
+    authProvider?: {
       name: TProvider;
       clientId: string;
       clientSecret: string;
@@ -93,120 +93,30 @@ export const app = <TProvider extends "google">(
     strict: true,
   });
 
-  app.use("/auth/login", async (c) => {
-    if (settings.authProvider.name === "google") {
-      try {
-        const url = new URL(c.req.url);
-        const code = url.searchParams.get("code");
+  const authProvider = settings.authProvider;
 
-        if (!code) throw new Error("Missing auth code");
+  if (authProvider) {
+    app.use("/auth/login", async (c) => {
+      if (authProvider.name === "google") {
+        try {
+          const url = new URL(c.req.url);
+          const code = url.searchParams.get("code");
 
-        const params = new URLSearchParams({
-          client_id: settings.authProvider.clientId,
-          redirect_uri: `${url.origin}/auth/login`,
-          client_secret: settings.authProvider.clientSecret,
-          scope: "email",
-          grant_type: "authorization_code",
-          access_type: "offline",
-          prompt: "consent",
-          code,
-        });
+          if (!code) throw new Error("Missing auth code");
 
-        const googleBaseUrl = "https://oauth2.googleapis.com";
-
-        const res = await fetch(
-          `${googleBaseUrl}/token`,
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-            method: "POST",
-            body: params,
-          },
-        );
-
-        if (res.ok) {
-          const responseSchema = v.object({
-            access_token: v.string(),
-            refresh_token: v.string(),
-            id_token: v.string(),
-            expires_in: v.number(),
-          });
-
-          const responseData = await res.json();
-
-          const tokens = v.parse(responseSchema, responseData);
-
-          const email = v.parse(
-            v.string(),
-            decode(tokens.id_token).payload.email,
-          );
-
-          const result = {
-            success: true,
-            accessToken: tokens.access_token,
-            refreshToken: tokens.refresh_token,
-            email,
-            expires_in: tokens.expires_in,
-            refresh_token_expires_in: 3600 * 72, // 3 days
-          };
-
-          setCookie(c, "access_token", result.accessToken, {
-            maxAge: result.expires_in,
-            httpOnly: true,
-            secure: true,
-            sameSite: "Lax",
-          });
-          setCookie(c, "refresh_token", result.refreshToken, {
-            maxAge: result.refresh_token_expires_in,
-            httpOnly: true,
-            secure: true,
-            sameSite: "Lax",
-          });
-          setCookie(c, "email", result.email, {
-            maxAge: result.refresh_token_expires_in,
-            httpOnly: true,
-            secure: true,
-            sameSite: "Lax",
-          });
-
-          await settings.authProvider.afterLoginHook?.(
-            result as AfterLoginHookTypes<TProvider>,
-          );
-
-          return c.redirect(settings.authProvider.redirectPathAfterLogin);
-        }
-        throw new Error("Failed to login");
-      } catch (error) {
-        const result = {
-          error,
-          success: false,
-        } as AfterLoginHookTypes<TProvider>;
-
-        await settings.authProvider.afterLoginHook?.(result);
-
-        return c.redirect(settings.authProvider.redirectPathAfterLogin);
-      }
-    }
-    throw new Error("Invalid auth provider");
-  });
-
-  app.use("/*", async (c, next) => {
-    if (settings.authProvider.name === "google") {
-      try {
-        const accessToken = getCookie(c, "access_token");
-        const refreshToken = getCookie(c, "refresh_token");
-        const email = getCookie(c, "email");
-
-        if (!accessToken && refreshToken && email) {
           const params = new URLSearchParams({
-            client_id: settings.authProvider.clientId,
-            client_secret: settings.authProvider.clientSecret,
-            grant_type: "refresh_token",
-            refresh_token: refreshToken,
+            client_id: authProvider.clientId,
+            redirect_uri: `${url.origin}/auth/login`,
+            client_secret: authProvider.clientSecret,
+            scope: "email",
+            grant_type: "authorization_code",
+            access_type: "offline",
+            prompt: "consent",
+            code,
           });
 
           const googleBaseUrl = "https://oauth2.googleapis.com";
+
           const res = await fetch(
             `${googleBaseUrl}/token`,
             {
@@ -221,18 +131,27 @@ export const app = <TProvider extends "google">(
           if (res.ok) {
             const responseSchema = v.object({
               access_token: v.string(),
+              refresh_token: v.string(),
               id_token: v.string(),
               expires_in: v.number(),
             });
 
-            const tokens = v.parse(responseSchema, await res.json());
+            const responseData = await res.json();
+
+            const tokens = v.parse(responseSchema, responseData);
+
+            const email = v.parse(
+              v.string(),
+              decode(tokens.id_token).payload.email,
+            );
 
             const result = {
               success: true,
               accessToken: tokens.access_token,
-              refreshToken,
+              refreshToken: tokens.refresh_token,
               email,
               expires_in: tokens.expires_in,
+              refresh_token_expires_in: 3600 * 72, // 3 days
             };
 
             setCookie(c, "access_token", result.accessToken, {
@@ -241,115 +160,131 @@ export const app = <TProvider extends "google">(
               secure: true,
               sameSite: "Lax",
             });
+            setCookie(c, "refresh_token", result.refreshToken, {
+              maxAge: result.refresh_token_expires_in,
+              httpOnly: true,
+              secure: true,
+              sameSite: "Lax",
+            });
+            setCookie(c, "email", result.email, {
+              maxAge: result.refresh_token_expires_in,
+              httpOnly: true,
+              secure: true,
+              sameSite: "Lax",
+            });
 
-            await settings.authProvider.refreshHook?.(
-              result as RefreshHookTypes<TProvider>,
+            await authProvider.afterLoginHook?.(
+              result as AfterLoginHookTypes<TProvider>,
             );
-          } else {
-            const result = {
-              success: false,
-              error: new Error("Failed to refresh token"),
-            } as RefreshHookTypes<TProvider>;
-            await settings.authProvider.refreshHook?.(result);
+
+            return c.redirect(authProvider.redirectPathAfterLogin);
           }
+          throw new Error("Failed to login");
+        } catch (error) {
+          const result = {
+            error,
+            success: false,
+          } as AfterLoginHookTypes<TProvider>;
+
+          await authProvider.afterLoginHook?.(result);
+
+          return c.redirect(authProvider.redirectPathAfterLogin);
         }
-      } catch (error) {
-        const result = {
-          success: false,
-          error,
-        } as RefreshHookTypes<TProvider>;
-        await settings.authProvider.refreshHook?.(result);
       }
-    }
-    await next();
-  });
+      throw new Error("Invalid auth provider");
+    });
 
-  app.use("/auth/logout", async (c) => {
-    if (settings.authProvider.name === "google") {
-      try {
-        const accessToken = getCookie(c, "access_token");
-        const refreshToken = getCookie(c, "refresh_token");
-        const email = getCookie(c, "email");
-
-        if (!refreshToken || !accessToken || !email) {
-          throw new Error("Missing tokens or email");
-        }
-
-        await settings.authProvider.beforeLogoutHook?.({
-          refreshToken,
-          accessToken,
-          email,
-        } as BeforeLogoutHookTypes<TProvider>);
-
-        // Clear cookies
-        setCookie(c, "access_token", "", {
-          maxAge: 0,
-          httpOnly: true,
-          secure: true,
-          sameSite: "Lax",
-        });
-        setCookie(c, "refresh_token", "", {
-          maxAge: 0,
-          httpOnly: true,
-          secure: true,
-          sameSite: "Lax",
-        });
-        setCookie(c, "email", "", {
-          maxAge: 0,
-          httpOnly: true,
-          secure: true,
-          sameSite: "Lax",
-        });
-
-        return c.redirect(settings.authProvider.redirectPathAfterLogout);
-      } catch (error) {
-        console.error("Error during logout:", error);
-        return c.redirect(settings.authProvider.redirectPathAfterLogout);
-      }
-    }
-    throw new Error("Invalid auth provider");
-  });
-
-  /** Validate Hook */
-  app.use("/*", async (c, next) => {
-    if (settings.authProvider.name === "google") {
-      const accessToken = getCookie(c, "access_token");
-      const refreshToken = getCookie(c, "refresh_token");
-      const email = getCookie(c, "email");
-
-      if (accessToken && refreshToken && email) {
+    app.use("/*", async (c, next) => {
+      if (authProvider.name === "google") {
         try {
-          const isValid = await settings.authProvider.validateHook?.({
-            accessToken,
-            refreshToken,
-            email,
-          } as ValidateHookTypes<TProvider>);
+          const accessToken = getCookie(c, "access_token");
+          const refreshToken = getCookie(c, "refresh_token");
+          const email = getCookie(c, "email");
 
-          if (isValid === false) {
-            // Clear all auth cookies if validation fails
-            setCookie(c, "access_token", "", {
-              maxAge: 0,
-              httpOnly: true,
-              secure: true,
-              sameSite: "Lax",
+          if (!accessToken && refreshToken && email) {
+            const params = new URLSearchParams({
+              client_id: authProvider.clientId,
+              client_secret: authProvider.clientSecret,
+              grant_type: "refresh_token",
+              refresh_token: refreshToken,
             });
-            setCookie(c, "refresh_token", "", {
-              maxAge: 0,
-              httpOnly: true,
-              secure: true,
-              sameSite: "Lax",
-            });
-            setCookie(c, "email", "", {
-              maxAge: 0,
-              httpOnly: true,
-              secure: true,
-              sameSite: "Lax",
-            });
-            return c.redirect(settings.authProvider.redirectPathAfterLogout);
+
+            const googleBaseUrl = "https://oauth2.googleapis.com";
+            const res = await fetch(
+              `${googleBaseUrl}/token`,
+              {
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+                method: "POST",
+                body: params,
+              },
+            );
+
+            if (res.ok) {
+              const responseSchema = v.object({
+                access_token: v.string(),
+                id_token: v.string(),
+                expires_in: v.number(),
+              });
+
+              const tokens = v.parse(responseSchema, await res.json());
+
+              const result = {
+                success: true,
+                accessToken: tokens.access_token,
+                refreshToken,
+                email,
+                expires_in: tokens.expires_in,
+              };
+
+              setCookie(c, "access_token", result.accessToken, {
+                maxAge: result.expires_in,
+                httpOnly: true,
+                secure: true,
+                sameSite: "Lax",
+              });
+
+              await authProvider.refreshHook?.(
+                result as RefreshHookTypes<TProvider>,
+              );
+            } else {
+              const result = {
+                success: false,
+                error: new Error("Failed to refresh token"),
+              } as RefreshHookTypes<TProvider>;
+              await authProvider.refreshHook?.(result);
+            }
           }
         } catch (error) {
-          console.error("Error in validate hook:", error);
-          // Clear cookies on error and redirect
+          const result = {
+            success: false,
+            error,
+          } as RefreshHookTypes<TProvider>;
+          await authProvider.refreshHook?.(result);
+        }
+      }
+      await next();
+    });
+
+    app.use("/auth/logout", async (c) => {
+      if (authProvider.name === "google") {
+        try {
+          const accessToken = getCookie(c, "access_token");
+          const refreshToken = getCookie(c, "refresh_token");
+          const email = getCookie(c, "email");
+
+          if (!refreshToken || !accessToken || !email) {
+            throw new Error("Missing tokens or email");
+          }
+
+          await authProvider.beforeLogoutHook?.({
+            refreshToken,
+            accessToken,
+            email,
+          } as BeforeLogoutHookTypes<TProvider>);
+
+          // Clear cookies
           setCookie(c, "access_token", "", {
             maxAge: 0,
             httpOnly: true,
@@ -368,39 +303,108 @@ export const app = <TProvider extends "google">(
             secure: true,
             sameSite: "Lax",
           });
-          return c.redirect(settings.authProvider.redirectPathAfterLogout);
+
+          return c.redirect(authProvider.redirectPathAfterLogout);
+        } catch (error) {
+          console.error("Error during logout:", error);
+          return c.redirect(authProvider.redirectPathAfterLogout);
         }
       }
-    }
-    await next();
-    return;
-  });
+      throw new Error("Invalid auth provider");
+    });
 
-  /** Context Variables */
-  app.use("/*", async (c, next) => {
-    if (settings.authProvider.name === "google") {
-      const refreshToken = getCookie(c, "refresh_token");
-      const email = getCookie(c, "email");
+    /** Validate Hook */
+    app.use("/*", async (c, next) => {
+      if (authProvider.name === "google") {
+        const accessToken = getCookie(c, "access_token");
+        const refreshToken = getCookie(c, "refresh_token");
+        const email = getCookie(c, "email");
 
-      const url = new URL(c.req.url);
+        if (accessToken && refreshToken && email) {
+          try {
+            const isValid = await authProvider.validateHook?.({
+              accessToken,
+              refreshToken,
+              email,
+            } as ValidateHookTypes<TProvider>);
 
-      // Set login URL
-      c.set(
-        "loginUrl",
-        `https://accounts.google.com/o/oauth2/v2/auth?client_id=${settings.authProvider.clientId}&redirect_uri=${url.origin}/auth/login&response_type=code&scope=email&access_type=offline&prompt=consent`,
-      );
+            if (isValid === false) {
+              // Clear all auth cookies if validation fails
+              setCookie(c, "access_token", "", {
+                maxAge: 0,
+                httpOnly: true,
+                secure: true,
+                sameSite: "Lax",
+              });
+              setCookie(c, "refresh_token", "", {
+                maxAge: 0,
+                httpOnly: true,
+                secure: true,
+                sameSite: "Lax",
+              });
+              setCookie(c, "email", "", {
+                maxAge: 0,
+                httpOnly: true,
+                secure: true,
+                sameSite: "Lax",
+              });
+              return c.redirect(authProvider.redirectPathAfterLogout);
+            }
+          } catch (error) {
+            console.error("Error in validate hook:", error);
+            // Clear cookies on error and redirect
+            setCookie(c, "access_token", "", {
+              maxAge: 0,
+              httpOnly: true,
+              secure: true,
+              sameSite: "Lax",
+            });
+            setCookie(c, "refresh_token", "", {
+              maxAge: 0,
+              httpOnly: true,
+              secure: true,
+              sameSite: "Lax",
+            });
+            setCookie(c, "email", "", {
+              maxAge: 0,
+              httpOnly: true,
+              secure: true,
+              sameSite: "Lax",
+            });
+            return c.redirect(authProvider.redirectPathAfterLogout);
+          }
+        }
+      }
+      await next();
+      return;
+    });
 
-      // Set logout URL
-      c.set("logoutUrl", "/auth/logout");
+    /** Context Variables */
+    app.use("/*", async (c, next) => {
+      if (authProvider.name === "google") {
+        const refreshToken = getCookie(c, "refresh_token");
+        const email = getCookie(c, "email");
 
-      // Set isLoggedIn
-      c.set("isLoggedIn", !!(refreshToken && email));
+        const url = new URL(c.req.url);
 
-      // Set email if available
-      c.set("email", email);
-    }
-    await next();
-  });
+        // Set login URL
+        c.set(
+          "loginUrl",
+          `https://accounts.google.com/o/oauth2/v2/auth?client_id=${authProvider.clientId}&redirect_uri=${url.origin}/auth/login&response_type=code&scope=email&access_type=offline&prompt=consent`,
+        );
+
+        // Set logout URL
+        c.set("logoutUrl", "/auth/logout");
+
+        // Set isLoggedIn
+        c.set("isLoggedIn", !!(refreshToken && email));
+
+        // Set email if available
+        c.set("email", email);
+      }
+      await next();
+    });
+  }
 
   /** Error Handling */
   app.onError((err, c) => {
