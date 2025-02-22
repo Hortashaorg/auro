144553297860e9e3144220e9160456b543da4584
Hortasha <eidemartin_303@hotmail.com>
2025-02-22 11:42:21 +0000

    add migration

---
+++ packages/database/drizzle/0004_lean_stone_men.sql
@@ -0,0 +1,18 @@
+DROP TABLE "asset" CASCADE;--> statement-breakpoint
+DROP TABLE "currency" CASCADE;--> statement-breakpoint
+DROP TABLE "item" CASCADE;--> statement-breakpoint
+DROP TABLE "location" CASCADE;--> statement-breakpoint
+DROP TABLE "location_npc" CASCADE;--> statement-breakpoint
+DROP TABLE "location_req_item" CASCADE;--> statement-breakpoint
+DROP TABLE "location_req_skill" CASCADE;--> statement-breakpoint
+DROP TABLE "location_reward_currency" CASCADE;--> statement-breakpoint
+DROP TABLE "location_reward_item" CASCADE;--> statement-breakpoint
+DROP TABLE "location_reward_skill" CASCADE;--> statement-breakpoint
+DROP TABLE "npc" CASCADE;--> statement-breakpoint
+DROP TABLE "skill" CASCADE;--> statement-breakpoint
+DROP TABLE "user_currency" CASCADE;--> statement-breakpoint
+DROP TABLE "user_item" CASCADE;--> statement-breakpoint
+DROP TABLE "user_skill" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."assetcategory";--> statement-breakpoint
+DROP TYPE "public"."itemtype";--> statement-breakpoint
+DROP TYPE "public"."rarity";
