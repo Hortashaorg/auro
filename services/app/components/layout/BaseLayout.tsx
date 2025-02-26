70ade13a0c35f7e2b1e86e3794c9029747a8e253
MartinEide <martin.eide@hent.no>
2025-02-26 11:46:26 +0000

    alpinejs version number

--- services/app/components/layout/BaseLayout.tsx
+++ services/app/components/layout/BaseLayout.tsx
@@ -21,7 +21,11 @@ export const BaseLayout: FC<Props> = ({
       <body className="dark:bg-background-950 bg-background-50" x-data="{}">
         {children}
         <script src="https://unpkg.com/htmx.org@2.0.4"></script>
-        <script src="//unpkg.com/alpinejs" defer></script>
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"
+        >
+        </script>
         <script src="/public/darkmode.js"></script>
       </body>
     </html>
