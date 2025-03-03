759c3af3b18b7c72d82ffc6eaf1d1436487c0e38
MartinEide <martin.eide@hent.no>
2025-03-03 13:54:24 +0000

    fix a type issue by throwing the error again properly

--- services/app/api/action/ChangeActionResourceRewards.tsx
+++ services/app/api/action/ChangeActionResourceRewards.tsx
@@ -2,9 +2,14 @@ import { createRoute, v } from "@kalena/framework";
 import { isAdminOfServer } from "@permissions/index.ts";
 import { throwError } from "@package/common";
 import { createEvents } from "@comp/utils/events.ts";
+import { ModifyResourceOfActionForm } from "@sections/forms/ModifyResourceOfActionForm.tsx";
+import { db, schema, sql } from "@package/database";
 
-const ChangeActionResourceRewards = () => {
+const ChangeActionResourceRewards = async () => {
   const context = changeActionResourceRewardsRoute.context();
+  const actionId = context.req.param("actionId");
+  if (!actionId) throwError("Missing actionId");
+
   const result = context.req.valid("form");
 
   if (!result.success) {
@@ -27,20 +32,84 @@ const ChangeActionResourceRewards = () => {
     return <p>Invalid form data</p>;
   }
 
-  context.header(
-    "HX-Trigger",
-    createEvents([
-      {
-        name: "form-error",
-        values: {
-          "resource_dbb6eac1-693b-46e0-a117-b5436dfae18b_max": "Mr li failed",
-        },
-      },
-    ]),
-  );
+  const resourceRewards = Object.entries(result.output);
 
-  console.log(result.output);
-  return <p>yolo</p>;
+  const res = resourceRewards.reduce((acc, [key, value]) => {
+    const resourceId = key.split("_")[1] ?? throwError("Missing key");
+    const field = key.split("_")[2] ?? throwError("Missing key");
+    const fieldValue = value as string;
+
+    if (acc[resourceId]) {
+      acc[resourceId][field] = fieldValue;
+    } else {
+      acc[resourceId] = {
+        id: resourceId,
+      };
+      acc[resourceId][field] = fieldValue;
+    }
+    return acc;
+  }, {} as Record<string, Record<string, string>>);
+
+  // Define schema for a single resource entry
+  const resourcesSchema = v.array(v.object({
+    id: v.pipe(v.string(), v.uuid()),
+    chance: v.pipe(
+      v.string(),
+      v.transform((val) => parseInt(val, 10)),
+      v.number(),
+      v.integer(),
+      v.minValue(0),
+      v.maxValue(100),
+    ),
+    min: v.pipe(
+      v.string(),
+      v.transform((val) => parseInt(val, 10)),
+      v.number(),
+      v.integer(),
+      v.minValue(1),
+    ),
+    max: v.pipe(
+      v.string(),
+      v.transform((val) => parseInt(val, 10)),
+      v.number(),
+      v.integer(),
+      v.minValue(1),
+    ),
+  }));
+
+  const parsedResult = v.parse(resourcesSchema, Object.values(res));
+
+  try {
+    // Query
+    await db.insert(schema.actionResourceReward).values(
+      parsedResult.map((entry) => {
+        return {
+          actionId: actionId,
+          resourceId: entry.id,
+          chance: entry.chance,
+          quantityMin: entry.min,
+          quantityMax: entry.max,
+        };
+      }),
+    ).onConflictDoUpdate({
+      set: {
+        chance: sql`excluded.chance`,
+        quantityMin: sql`excluded.quantity_min`,
+        quantityMax: sql`excluded.quantity_max`,
+        updatedAt: sql`now()`,
+      },
+      target: [
+        schema.actionResourceReward.actionId,
+        schema.actionResourceReward.resourceId,
+      ],
+    });
+
+    return <ModifyResourceOfActionForm hx-swap-oob="true" />;
+  } catch (error) {
+    console.error("Error updating resources:", error);
+
+    throw error;
+  }
 };
 
 const endPattern = v.custom<`resource_${string}_${string}`>(
