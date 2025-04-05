7ae9f1d97d725d8862c028c205ac140b7942502c
Hortasha <eidemartin_303@hotmail.com>
2025-04-05 07:57:32 +0000

    tweaks

--- services/app/routes/servers/[serverId]/resources/ResourcesTable.section.tsx
+++ services/app/routes/servers/[serverId]/resources/ResourcesTable.section.tsx
@@ -1,5 +1,3 @@
-import { Title } from "@comp/atoms/typography/index.ts";
-import { Section } from "@comp/atoms/layout/index.ts";
 import {
   Table,
   TableBody,
@@ -17,34 +15,31 @@ export const ResourcesTable = async ({ serverId, ...props }: Props) => {
   const resources = await getUserResources(serverId);
 
   return (
-    <Section id="player-resources" {...props}>
-      <Title level="h2">Resources</Title>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableCell isHeader>Resource</TableCell>
-            <TableCell isHeader>Name</TableCell>
-            <TableCell isHeader>Description</TableCell>
-            <TableCell isHeader>Quantity</TableCell>
+    <Table id="player-resources" {...props}>
+      <TableHeader>
+        <TableRow>
+          <TableCell isHeader>Resource</TableCell>
+          <TableCell isHeader>Name</TableCell>
+          <TableCell isHeader>Description</TableCell>
+          <TableCell isHeader>Quantity</TableCell>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {resources.map((resource) => (
+          <TableRow key={resource.resource.id}>
+            <TableCell>
+              <img
+                src={resource.asset.url}
+                alt={`${resource.resource.name} icon`}
+                className="w-8 h-8 object-cover rounded"
+              />
+            </TableCell>
+            <TableCell>{resource.resource.name}</TableCell>
+            <TableCell>{resource.resource.description}</TableCell>
+            <TableCell>{resource.user_resource.quantity}</TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {resources.map((resource) => (
-            <TableRow key={resource.resource.id}>
-              <TableCell>
-                <img
-                  src={resource.asset.url}
-                  alt={`${resource.resource.name} icon`}
-                  className="w-8 h-8 object-cover rounded"
-                />
-              </TableCell>
-              <TableCell>{resource.resource.name}</TableCell>
-              <TableCell>{resource.resource.description}</TableCell>
-              <TableCell>{resource.user_resource.quantity}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Section>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
