182b7bcdc06891f36bf613374b3aecc9c8762757
MartinEide <martin.eide@hent.no>
2025-04-15 07:34:45 +0000

    create test of creating items

---
+++ services/app/routes/servers/[serverId]/admin/items/Items.spec.ts
@@ -0,0 +1,60 @@
+import { expect, type Page, test } from "@playwright/test";
+
+test("@target Create item functionality", async ({ page }) => {
+  const cwd = Deno.cwd();
+  const { loginAs } = await import(
+    `file:///${cwd}/playwright/login.ts`
+  ) as {
+    loginAs: (page: Page, user: "admin" | "player") => Promise<void>;
+  };
+
+  await loginAs(page, "admin");
+  await page.getByRole("link", { name: "Servers" }).first().click();
+  await page.waitForURL("http://localhost:4000/servers");
+
+  await page.locator("section").filter({ hasText: "My ServersPopulated Test" })
+    .getByRole("button").click();
+  await page.getByRole("button", { name: "Configuration" }).click();
+  await page.getByRole("link", { name: "Items" }).click();
+  await page.waitForTimeout(300);
+
+  await page.getByRole("button", { name: "Create Item" }).click();
+
+  await page.getByRole("textbox", { name: "Item Name*" }).fill(
+    "Test Item",
+  );
+  await page.getByRole("textbox", { name: "Description" }).fill(
+    "Test item description",
+  );
+  await page.getByLabel("Rarity*").selectOption("legendary");
+  await page.getByRole("img", { name: "Sword 3" }).click();
+  await page.getByRole("dialog").getByRole("button", {
+    name: "Create Item",
+  }).click();
+
+  const itemName = page.getByText("Test Item").first();
+
+  await expect(itemName).toBeVisible();
+
+  await page.close();
+});
+
+test("Player cannot access items admin page", async ({ page }) => {
+  const cwd = Deno.cwd();
+  const { loginAs } = await import(
+    `file:///${cwd}/playwright/login.ts`
+  ) as {
+    loginAs: (page: Page, user: "admin" | "player") => Promise<void>;
+  };
+
+  await loginAs(page, "player");
+
+  await page.goto(
+    "http://localhost:4000/servers/5bbcb026-e240-48d8-b66d-7105df74cf9f/admin/items",
+  );
+
+  await page.waitForURL("http://localhost:4000/servers");
+  expect(page.url()).not.toContain("/admin");
+
+  await page.close();
+});
