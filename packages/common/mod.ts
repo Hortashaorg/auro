a3d7841efd2d09ea76d3a3dd5495bae7015fd50d
Hortasha <eidemartin_303@hotmail.com>
2025-05-31 09:59:26 +0000

    upgrade packages

--- packages/common/mod.ts
+++ packages/common/mod.ts
@@ -1,5 +1,5 @@
-import { crypto } from "@std/crypto";
-import { encodeHex } from "@std/encoding/hex";
+import { crypto } from "jsr:@std/crypto";
+import { encodeHex } from "jsr:@std/encoding/hex";
 
 export const throwError = (message: string): never => {
   throw new Error(message);
@@ -10,6 +10,5 @@ export const hashString = async (str: string): Promise<string> => {
   const data = encoder.encode(str);
 
   const buffer = await crypto.subtle.digest("SHA-256", data);
-
   return encodeHex(buffer);
 };
