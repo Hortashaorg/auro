2742bec6e29c95e3b22db79c7ca664f1f5187877
Hortasha <eidemartin_303@hotmail.com>
2025-06-03 16:01:47 +0000

    use new queries in contexts

--- packages/database/queries/users/getUserByEmail.ts
+++ packages/database/queries/users/getUserByEmail.ts
@@ -1,17 +1,23 @@
 import { and, db, eq, schema } from "@db/mod.ts";
 
 export const getUserByEmail = async (email: string, gameId: string) => {
-  const [user] = await db.select().from(schema.user).innerJoin(
-    schema.game,
-    eq(schema.user.gameId, schema.game.id),
-  ).innerJoin(
-    schema.account,
-    eq(schema.user.accountId, schema.account.id),
-  ).where(
-    and(
-      eq(schema.game.id, gameId),
-      eq(schema.account.email, email),
-    ),
-  );
+  const [user] = await db
+    .select()
+    .from(schema.user)
+    .innerJoin(
+      schema.game,
+      eq(schema.user.gameId, schema.game.id),
+    )
+    .innerJoin(
+      schema.account,
+      eq(schema.user.accountId, schema.account.id),
+    )
+    .innerJoin(schema.location, eq(schema.location.id, schema.user.locationId))
+    .where(
+      and(
+        eq(schema.game.id, gameId),
+        eq(schema.account.email, email),
+      ),
+    );
   return user;
 };
